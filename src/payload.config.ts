81e19e5fb86f595961aef0fadd5de4354106952d
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-05 00:41:37 +0500

    auth integration p1

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,10 +7,13 @@ import sharp from 'sharp'
 // collections
 import AdminUsers from './app/(payload)/collections/AdminUsers'
 import ApiKeys from './app/(payload)/collections/ApiKeys'
-import Users from './app/(payload)/collections/Users'
+import Users from './app/(payload)/collections/Users';
+import { syncUser } from './services/syncUser'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+
+console.log('dirname', dirname)
 
 export default buildConfig({
   editor: lexicalEditor({}),
@@ -21,13 +24,13 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  // endpoints: [
-  //   {
-  //     path: '/sync-user',
-  //     method: 'post'
-  //     handler: () => {},
-  //   }
-  // ],
+  endpoints: [
+    {
+      path: '/sync-user',
+      method: 'post',
+      handler: syncUser,
+    },
+  ],
 
   // Define and configure your collections in this array
   collections: [AdminUsers, Users, ApiKeys],
