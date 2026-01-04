be336262c01a7f18f87e6b41d7e9079d946e7a21
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-04 23:11:10 +0500

    integrate payload p1

---
+++ src/app/(frontend)/result/page.tsx
@@ -0,0 +1,9 @@
+import GlobalPlayground from '@/components/playground/GlobalPlayground'
+
+export default function ResultPage() {
+  return (
+    <main className='h-screen w-full overflow-hidden'>
+      <GlobalPlayground />
+    </main>
+  )
+}
