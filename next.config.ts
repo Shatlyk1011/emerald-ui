bad94c60aacb7be703a377e29a8d04b20f39816e
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-06 01:50:10 +0500

    install docusarus

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from '@payloadcms/next/withPayload';
+import { createMDX } from 'fumadocs-mdx/next'
 import type { NextConfig } from 'next'
+
+const withMDX = createMDX()
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  reactStrictMode: true,
+  // cacheComponents: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 }
 
-export default withPayload(nextConfig)
+export default withPayload(withMDX(nextConfig))
