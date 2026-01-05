8cc06596997a743fee36970d7e4292d6d8a2dcd0
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-06 01:50:25 +0500

    integrate docusarus p-1

--- src/app/(frontend)/(main)/components/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Components() {
-  return (
-    <div className='flex h-full flex-1 items-start justify-center px-20 pt-30 font-sans'>
-      components page
-    </div>
-  )
-}
--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,6 +2,7 @@ import { Auth0Provider } from '@auth0/nextjs-auth0'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
+import { RootProvider } from "fumadocs-ui/provider/next";
 import './globals.css'
 
 const geistSans = Geist({
@@ -29,14 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Auth0Provider>{children}</Auth0Provider>
-        </ThemeProvider>
+        <RootProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Auth0Provider>{children}</Auth0Provider>
+          </ThemeProvider>
+        </RootProvider>
       </body>
     </html>
   )
