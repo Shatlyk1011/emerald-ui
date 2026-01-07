316afe8e1ea0f3d4114fa874d58b0f65170a4a97
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-08 01:36:04 +0500

    add bg threads with ogl

--- src/app/(frontend)/(main)/page.tsx
+++ src/app/(frontend)/(main)/page.tsx
@@ -1,8 +1,9 @@
 import { FileUploadInput } from '@/components/FileUpload'
+import BackgroundThreads from '@/components/ui/background-threads'
 
 export default function Home() {
   return (
-    <main className='mx-auto mb-16 flex h-full w-full max-w-5xl items-center justify-center px-12 pt-30 max-lg:px-8 max-sm:px-4 max-sm:pt-2'>
+    <main className='mx-auto mb-16 flex h-full w-full max-w-5xl min-h-[200vh] items-top justify-center px-12 pt-30 max-lg:px-8 max-sm:px-4 max-sm:pt-2 overflow-x-hidden'>
       <section className='flex w-full flex-col items-center gap-8'>
         <div className='text-center'>
           <h1 className='-tracking-two mb-2 text-4xl font-semibold'>
@@ -14,6 +15,9 @@ export default function Home() {
         </div>
         <FileUploadInput />
       </section>
+      <div className='absolute top-0 left-0 w-full h-svh -z-1'>
+        <BackgroundThreads />
+      </div> 
     </main>
   )
 }
