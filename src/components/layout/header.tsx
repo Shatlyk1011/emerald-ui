320bdf585907a2f70f5663d9c92517c7065b80f5
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-08 01:05:49 +0500

    add inspiration page component p2

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,8 @@ import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import ThemeToggle from '@/components/ui/theme-toggle'
 
-const { components, inspiration } = {
+const { components, inspiration, home } = {
+  home: '/',
   inspiration: '/inspiration',
   components: '/docs',
 }
@@ -14,44 +15,59 @@ const Header = () => {
   const pathname = usePathname()
 
   return (
-    <header className='flex items-center justify-between border-b px-8 py-2 font-sans'>
-      <Link href='/' className='w-20 max-sm:min-w-8'>
-        <span className=''>Logo</span>
-      </Link>
+    <div className='border-b border-border'>
+      <header className='flex items-center justify-between max-w-7xl mx-auto w-full  px-8 py-2 font-sans'>
+        <Link href='/' className='w-20 max-sm:min-w-8'>
+          <span className=''>Logo</span>
+        </Link>
 
-      <nav className='flex flex-1 justify-center'>
-        <ul className='-tracking-one flex items-center text-sm font-medium max-sm:text-sm'>
-          <li>
-            <Link
-              href={inspiration}
-              className={cn(
-                'px-4 py-2 transition max-sm:px-2',
-                pathname === inspiration &&
+        <nav className='flex flex-1 justify-center'>
+          <ul className='-tracking-one flex items-center text-sm font-medium max-sm:text-sm'>
+            <li>
+              <Link
+                href={home}
+                className={cn(
+                  'px-4 py-2 transition max-sm:px-2',
+                  pathname === home &&
                   'text-chart-2 font-medium opacity-100'
-              )}
-            >
-              Inspiration
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={components}
-              className={cn(
-                'px-4 py-2.5 transition max-sm:px-2',
-                pathname === components &&
+                )}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={inspiration}
+                className={cn(
+                  'px-4 py-2 transition max-sm:px-2',
+                  pathname === inspiration &&
                   'text-chart-2 font-medium opacity-100'
-              )}
-            >
-              Node UI
-            </Link>
-          </li>
-        </ul>
-      </nav>
+                )}
+              >
+                Inspiration
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={components}
+                className={cn(
+                  'px-4 py-2.5 transition max-sm:px-2',
+                  pathname === components &&
+                  'text-chart-2 font-medium opacity-100'
+                )}
+              >
+                Node UI
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-      <div className='flex w-20 justify-end max-sm:w-14'>
-        <ThemeToggle />
-      </div>
-    </header>
+        <div className='flex w-20 justify-end max-sm:w-14'>
+          <ThemeToggle />
+        </div>
+      </header>
+    </div>
+
   )
 }
 export default Header
