e066887dbc403de01ef2f59fe6e815c1bf992a34
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-08 01:04:49 +0500

    add inspiration page

--- src/app/(frontend)/(main)/inspiration/page.tsx
+++ src/app/(frontend)/(main)/inspiration/page.tsx
@@ -1,7 +1,34 @@
-export default function Inspiration() {
+import { MOCK_SITES } from '../../../../../public/mockData'
+
+// components
+import FilterSection from '@/components/InspirationPage/FilterSection'
+import SiteCard from '@/components/InspirationPage/SiteCard'
+
+export default function InspirationPage() {
   return (
-    <div className='flex h-full flex-1 items-start justify-center px-20 pt-30 font-sans'>
-      inspiration page
+    <div className="min-h-screen bg-background text-white font-sans selection:bg-white/20">
+      <main className="max-w-7xl mx-auto px-6 py-10">
+        {/* <FilterSection /> */}
+
+        <h2 className="mb-6 text-2xl font-semibold -tracking-two">
+          Explore curated websites
+        </h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-3 gap-y-6">
+          {[...MOCK_SITES, ...MOCK_SITES].map((site, i) => (
+            <SiteCard
+              key={i}
+              title={site.title}
+              description={site.description}
+              imageColor={site.imageColor}
+              logoColor={site.logoColor}
+              logoLetter={site.logoLetter}
+              isNew={site.isNew}
+              isLocked={site.isLocked}
+            />
+          ))}
+        </div>
+      </main>
     </div>
   )
 }
--- src/app/(frontend)/(main)/page.tsx
+++ src/app/(frontend)/(main)/page.tsx
@@ -2,7 +2,7 @@ import { FileUploadInput } from '@/components/FileUpload'
 
 export default function Home() {
   return (
-    <section className='mx-auto mb-16 flex h-full w-full max-w-5xl items-center justify-center px-12 pt-30 max-lg:px-8 max-sm:px-4 max-sm:pt-2'>
+    <main className='mx-auto mb-16 flex h-full w-full max-w-5xl items-center justify-center px-12 pt-30 max-lg:px-8 max-sm:px-4 max-sm:pt-2'>
       <section className='flex w-full flex-col items-center gap-8'>
         <div className='text-center'>
           <h1 className='-tracking-two mb-2 text-4xl font-semibold'>
@@ -14,6 +14,6 @@ export default function Home() {
         </div>
         <FileUploadInput />
       </section>
-    </section>
+    </main>
   )
 }
