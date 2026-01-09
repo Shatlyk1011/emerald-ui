646dab9934b87d776657fa2456558ee4f67c52de
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-09 18:17:54 +0500

    add inspiration collections

---
+++ src/app/(payload)/collections/InspirationWebsites/index.ts
@@ -0,0 +1,54 @@
+import { CollectionConfig } from 'payload'
+
+const InspirationWebsites: CollectionConfig = {
+  slug: 'inpiration-websites',
+  admin: {
+    useAsTitle: 'Title',
+  },
+  fields: [
+    {
+      name: 'Title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'category',
+      type: 'text',
+      required: false,
+    },
+    {
+      name: 'style',
+      type: 'text',
+      hasMany: true,
+      required: true,
+    },
+    {
+      name: 'mode',
+      type: 'select',
+      options: [
+        { label: 'Dark', value: 'dark' },
+        { label: 'Light', value: 'light' },
+        { label: 'Hybrid', value: 'hybrid' },
+      ],
+      required:false
+    },
+    {
+      name: 'imgUrl',
+      type: 'text',
+      required: false,
+    },
+    {
+      name: 'faviconUrl',
+      type: 'text',
+      required: false,
+    },
+    
+  ],
+}
+
+export default InspirationWebsites
