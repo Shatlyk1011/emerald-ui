270adbf9cad96aab6a65d2cde06e797c43f4770e
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-21 17:55:43 +0500

    update collection order

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,15 +1,29 @@
-import path from 'path'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import { fileURLToPath } from 'node:url'
-import { buildConfig } from 'payload'
-import sharp from 'sharp'
-import AdminUsers from './app/(payload)/collections/AdminUsers'
+import path from 'path';
+import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { lexicalEditor } from '@payloadcms/richtext-lexical';
+import { fileURLToPath } from 'node:url';
+import { buildConfig } from 'payload';
+import sharp from 'sharp';
+import AdminUsers from './app/(payload)/collections/AdminUsers';
 // collections
-import Categories from './app/(payload)/collections/Categories'
-import InspirationWebsites from './app/(payload)/collections/InspirationWebsites'
-import Media from './app/(payload)/collections/Media'
-import WebsiteStyle from './app/(payload)/collections/WebsiteStyle'
+import Categories from './app/(payload)/collections/Categories';
+import InspirationWebsites from './app/(payload)/collections/InspirationWebsites';
+import Media from './app/(payload)/collections/Media';
+import WebsiteStyle from './app/(payload)/collections/WebsiteStyle';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -44,8 +58,8 @@ export default buildConfig({
 
   // Define and configure your collections in this array
   collections: [
-    Media,
     InspirationWebsites,
+    Media,
     Categories,
     WebsiteStyle,
     AdminUsers,
