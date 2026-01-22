dc03583e401a46e6c4d423924e0c6c7d8ed88f1e
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-22 16:49:53 +0500

    integrate infinite scroll query

--- src/services/useGetInspirationSites.ts
+++ src/services/useGetInspirationSites.ts
@@ -1,39 +1,53 @@
-import { AxiosResponse } from 'axios'
-import { useQuery } from '@tanstack/react-query'
-import { InspirationWebsite } from '@/payload-types'
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { IWebsites } from '@/types/inspiration'
+import { Where } from 'payload'
+import { stringify } from 'qs-esm'
 import { axios } from '@/lib/axios'
 
-export const useGetWebsites = () => {
-  const getWebsite = async (id: string): Promise<InspirationWebsite> => {
-    return (await axios(`/inspiration-websites/${id}`)).data
-  }
+// export const useGetWebsites = () => {
+//   const getWebsite = async (id: string): Promise<InspirationWebsite> => {
+//     return (await axios(`/inspiration-websites/${id}`)).data
+//   }
 
-  const getWebsites = async (query?: string): Promise<IWebsites> => {
-    const response: AxiosResponse<IWebsites> = await axios(
-      `/inspiration-websites/${query || ''}`
-    )
+//   const getWebsites = async (query?: string): Promise<IWebsites> => {
+//     const response: AxiosResponse<IWebsites> = await axios(
+//       `/inspiration-websites/${query || ''}`
+//     )
 
-    return response.data
-  }
+//     return response.data
+//   }
 
-  return { getWebsite, getWebsites }
-}
+//   return { getWebsite, getWebsites }
+// }
 
-//React Query
-export const useWebsitesQuery = (initialData: InspirationWebsite[]) => {
-  const { data, isLoading } = useQuery<InspirationWebsite[]>({
-    queryFn: async () => {
-      const { data: websites } = await axios({
-        url: '/inspiration-websites',
-      })
 
-      return websites
+
+// Infinite Query for Inspiration Sites
+export const useInfiniteInspirationSites = (
+  initialData: IWebsites,
+  query?: Where
+) => {
+  return useInfiniteQuery<IWebsites>({
+    queryKey: ['inspiration-sites', query],
+    queryFn: async ({ pageParam = 1 }) => {
+      const stringifiedQuery = stringify(
+        {
+          where: query || { isVisible: { equals: true } },
+          depth: 1,
+          limit: 12,
+          page: pageParam,
+        },
+        { addQueryPrefix: true }
+      )
+      const response = await axios(`/inspiration-websites${stringifiedQuery}`)
+      return response.data
     },
-    initialData,
-    staleTime: Infinity,
-    queryKey: ['websites'],
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) =>
+      lastPage.hasNextPage ? lastPage.nextPage : undefined,
+    initialData: {
+      pages: [initialData],
+      pageParams: [1],
+    },
   })
-
-  return { websites: data, isLoading }
 }
