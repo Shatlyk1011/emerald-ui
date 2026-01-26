b4e301fb93788ef05a276812268602a59b61da6f
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-26 16:15:04 +0500

    reorder api files

--- src/components/auth/LoginPage.tsx
+++ src/components/auth/LoginPage.tsx
@@ -26,7 +26,7 @@ export default function LoginPage() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider,
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `${window.location.origin}/api/auth/callback`,
         },
       })
 
@@ -61,7 +61,7 @@ export default function LoginPage() {
       const { error } = await supabase.auth.signInWithOtp({
         email: result.data.email,
         options: {
-          emailRedirectTo: `${window.location.origin}/auth/callback`,
+          emailRedirectTo: `${window.location.origin}/api/auth/callback`,
         },
       })
 
