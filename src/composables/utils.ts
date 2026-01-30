5d90f083c0f61e587cc133feafa0b4b262902fc9
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-01-30 20:52:34 +0500

    fix profile fetching implementation

--- src/composables/utils.ts
+++ src/composables/utils.ts
@@ -1,12 +1,20 @@
 export function debounce<T>(func: (...args: T[]) => void, delay: number) {
-  let timeoutId: undefined | ReturnType<typeof setTimeout>;
+  let timeoutId: undefined | ReturnType<typeof setTimeout>
 
   return function (...args: T[]) {
-    clearTimeout(timeoutId);
+    clearTimeout(timeoutId)
 
     timeoutId = setTimeout(() => {
       // @ts-expect-error valid
-      func.apply(this, args);
-    }, delay);
-  };
+      func.apply(this, args)
+    }, delay)
+  }
+}
+
+export const formatDate = (dateString: string) => {
+  return new Date(dateString).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
 }
