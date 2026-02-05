5bc79484df318e0ffa7b307d6205f6942670dd1f
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-02-05 22:57:22 +0500

    fix dialog max width issue

--- src/app/(frontend)/(main)/node-inspiration/page.tsx
+++ src/app/(frontend)/(main)/node-inspiration/page.tsx
@@ -5,7 +5,7 @@ import InspirationPageSkeleton from '@/components/InspirationPage/InspirationPag
 export default async function InspirationPage() {
   return (
     <main className='bg-background min-h-screen font-sans'>
-      <div className='mx-auto max-w-520 px-10 py-10'>
+      <div className='mx-auto max-w-480 px-10 py-10'>
         <Suspense fallback={<InspirationPageSkeleton />}>
           <InspirationPageContent />
         </Suspense>
