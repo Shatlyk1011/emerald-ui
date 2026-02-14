274a8bcdb380093363948f8648fdeef3dd9b02a9
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-02-14 23:55:07 +0500

    clean up

--- src/components/_providers/tanstack-query.tsx
+++ src/components/_providers/tanstack-query.tsx
@@ -1,14 +1,26 @@
 'use client'
 import { FC, ReactNode, useState } from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { QueryClientConfig } from '@tanstack/react-query'
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { QueryClient } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import { get, set, del } from 'idb-keyval'
 
-export const QueryConfig: QueryClientConfig = {
+const persister = createAsyncStoragePersister({
+  storage: {
+    getItem: get,
+    setItem: set,
+    removeItem: del,
+  },
+})
+
+export const QueryConfig = {
   defaultOptions: {
     queries: {
+      gcTime: 1000 * 60 * 60 * 72, // 72 hours
+      // UPDATE IT TO 30 MIN 
+      staleTime: 0, // 0 minutes (immediate revalidation)
       refetchOnWindowFocus: false,
     },
-
     mutations: {
       onError: (error: Error) => {
         /** we can use toast or notification here */
@@ -24,8 +36,14 @@ interface Props {
 
 const TanstackQueryProvider: FC<Props> = ({ children }) => {
   const [queryClient] = useState(() => new QueryClient(QueryConfig))
+
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
+      {children}
+    </PersistQueryClientProvider>
   )
 }
 export default TanstackQueryProvider
