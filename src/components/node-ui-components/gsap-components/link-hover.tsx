be1a3353d6f45d68adc099359fb0b8955f3d8ad6
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-02-16 20:09:03 +0500

    update link hover props

--- src/components/node-ui-components/gsap-components/link-hover.tsx
+++ src/components/node-ui-components/gsap-components/link-hover.tsx
@@ -12,17 +12,26 @@ import { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
-const defaultImg = "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/emilie.webp"
+const DefaultImg = "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/emilie.webp"
 
-const images = [
-  "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/1820productions.webp",
-  "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/telhaclarke.webp",
-  "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/jonasreymondin.webp",
-  "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/www-anima-ai.webp",
-  "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/dulcedo-com-.webp",
+const DefaultItems: Item[] = [
+  { imgUrl: "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/1820productions.webp", title: 'Home' },
+  { imgUrl: "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/telhaclarke.webp", title: 'Blog' },
+  { imgUrl: "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/jonasreymondin.webp", title: 'About' },
+  { imgUrl: "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/www-anima-ai.webp", title: 'Projects' },
+  { imgUrl: "https://pyengguphcmeqlelpozr.supabase.co/storage/v1/object/public/images/dulcedo-com-.webp", title: 'Contacts' },
 ]
 
-export default function ImageHover() {
+type Item = {
+  imgUrl: string
+  title: string
+}
+
+interface Props {
+  items: Item[]
+}
+
+export default function ImageHover({ items = DefaultItems }: Props) {
   const sectionRef = useRef(null);
   const previewContainerRef = useRef<HTMLDivElement>(null);
   const newImgRef = useRef<HTMLDivElement>(null);
@@ -39,7 +48,7 @@ export default function ImageHover() {
         const imgContainer = document.createElement("div");
         imgContainer.classList.add("temp-image", "absolute", "rotate-[-30deg]", "-left-1/2", "top-[125%]");
         const img = document.createElement("img");
-        img.src = images[index];
+        img.src = items[index].imgUrl;
         img.alt = "";
         img.classList.add("w-full", "h-full", 'object-fill')
         imgContainer.appendChild(img);
@@ -91,18 +100,16 @@ export default function ImageHover() {
     <section ref={sectionRef} className='flex items-center gap-16 w-full h-full p-20 max-md:p-5'>
       <div className='flex-1 '>
         <ul className='flex flex-col  gap-6 text-5xl max-md:text-3xl max-md:gap-2  font-medium text-muted-foreground [&>li]:transition [&>li:hover]:text-foreground flex-1'>
-          <li className="menu-link-item">Home</li>
-          <li className="menu-link-item">Blog</li>
-          <li className="menu-link-item">About</li>
-          <li className="menu-link-item">Projects</li>
-          <li className="menu-link-item">Contact</li>
+          {items.map(({ title }) => (
+            <li key={title} className="menu-link-item">{title}</li>
+          ))}
         </ul>
       </div>
       <div ref={previewContainerRef} className="flex-2 max-w-90 max-md:max-w-64 min-w-48 rotate-15 left-1/2 [clip-path:polygon(0_0,100%_0,100%_100%,0%_100%)]">
-        <img src={defaultImg} className='w-full h-full object-fill' alt="" />
+        <img src={DefaultImg} className='w-full h-full object-fill' alt="" />
 
         <div ref={newImgRef} className="rotate-[-30deg] absolute w-full h-full -left-1/2 top-[125%]">
-          <img src={images[1]} className='w-full h-full object-fill' alt="" />
+          <img src={items[1].imgUrl} className='w-full h-full object-fill' alt="" />
         </div>
       </div>
     </section>
