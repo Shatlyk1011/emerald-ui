c527d7f3a7132b87d4af8de64bb74d06de2221bf
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-02-27 12:49:03 +0500

    add footer links

--- src/components/layout/footerLinks.ts
+++ src/components/layout/footerLinks.ts
@@ -1,21 +1,46 @@
-
 export const MotionComponents = [
+  { title: '3D Marquee', href: '/docs/components/3d-marquee' },
+  { title: 'Animated Dropdown', href: '/docs/components/animated-dropdown' },
+  { title: 'Animated Input', href: '/docs/components/animated-input' },
+  { title: 'Animated Number', href: '/docs/components/animated-number' },
+  { title: 'Buttons', href: '/docs/components/buttons' },
+  { title: 'Cards', href: '/docs/components/cards' },
+  { title: 'Flip Card', href: '/docs/components/flip-card' },
+  { title: 'Footer', href: '/docs/components/footer' },
   {
-    title: '3D Card Effect',
-    href: ''
-  }
+    title: 'Gradient File Upload',
+    href: '/docs/components/gradient-file-upload',
+  },
+  { title: 'Hero', href: '/docs/components/hero' },
+  { title: 'Parallax Image', href: '/docs/components/parallax-image' },
+  { title: 'Pricing', href: '/docs/components/pricing' },
+  { title: 'Subscribe Input', href: '/docs/components/subscribe-input' },
+  { title: 'Text', href: '/docs/components/text' },
 ]
 
 export const GsapComponents = [
   {
-    title: '3D Card Effect',
-    href: ''
-  }
+    title: 'Color Text Reveal',
+    href: '/docs/gsap/components/color-text-reveal',
+  },
+  { title: 'Fancy Text Hover', href: '/docs/gsap/components/fancy-text-hover' },
+  { title: 'Interactive Team', href: '/docs/gsap/components/interactive-team' },
+  { title: 'Link Hover', href: '/docs/gsap/components/link-hover' },
+  { title: 'Magnetic Effect', href: '/docs/gsap/components/magnetic-effect' },
+  {
+    title: 'Scroll Text Reveal V2',
+    href: '/docs/gsap/components/scroll-text-reveal-v2',
+  },
+  {
+    title: 'Scroll Text Reveal',
+    href: '/docs/gsap/components/scroll-text-reveal',
+  },
+  { title: 'Text Reveal', href: '/docs/gsap/components/text-reveal' },
 ]
 
 export const UsefulLinks = [
-  {
-    title: '3D Card Effect',
-    href: ''
-  }
+  { title: 'Home', href: '/' },
+  { title: 'Motion Components', href: '/docs' },
+  { title: 'GSAP Components', href: '/docs' },
+  { title: 'Github', href: 'https://github.com/shatlyk1011' },
 ]
