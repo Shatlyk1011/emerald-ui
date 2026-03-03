868aea9e7d4e426b05ca7a429a6e590673ceebe6
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-03-03 11:08:58 +0500

    fix logo sizing issue

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -18,9 +18,9 @@ const Logo: FC<Props> = ({ classes, size, logoColor }) => {
     >
       <LogoIcon
         className={cn(
-          'min-w-7 max-w-7',
+          'min-w-7 max-w-7 min-h-7 max-h-7',
           logoColor || 'current',
-          size === 'xl' && 'size-10'
+          size === 'xl' && 'min-w-10 max-w-10 min-h-10 max-h-10'
         )}
       />
       <span
