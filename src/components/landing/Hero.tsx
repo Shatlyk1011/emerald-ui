a36f9a2a4defe7686b40a40b475bbff98db6daef
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-03-11 00:22:18 +0500

    update hero component

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero({ totalDocs, images }: Props) {
     <section className=' flex items-center justify-between gap-10 px-20 max-2xl:px-6 max-xl:flex-col max-xl:items-start max-xl:gap-0 max-xl:px-0 max-lg:pt-6 '>
       <div className='relative flex w-full flex-3 flex-col items-start bg-cyan-50/0'>
         <span className='text-muted-foreground mb-1 text-xs'>
-          Websites - {totalDocs}+ | Components - {siteConfig.totalComponents}
+          Websites {totalDocs}+ | Components {siteConfig.totalComponents}
         </span>
         <h1 className='-tracking-two mb-2 text-4xl font-semibold'>
           <span className='text-nowrap'>Inspiration Websites &</span>
