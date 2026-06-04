f002ce723a0f9ee30ec0ab00ccc08192794c7b75
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2026-06-04 22:00:26 +0500

    feat: update component navigation links and expand GSAP documentation section

--- src/components/layout/footerLinks.ts
+++ src/components/layout/footerLinks.ts
@@ -6,7 +6,6 @@ export const MotionComponents = [
   { title: 'Buttons', href: '/docs/components/buttons' },
   { title: 'Cards', href: '/docs/components/cards' },
   { title: 'Flip Card', href: '/docs/components/flip-card' },
-  { title: 'Tooltip Cat', href: '/docs/components/tooltip-cat' },
   { title: 'Footer', href: '/docs/components/footer' },
   {
     title: 'Gradient File Upload',
@@ -17,6 +16,8 @@ export const MotionComponents = [
   { title: 'Pricing', href: '/docs/components/pricing' },
   { title: 'Subscribe Input', href: '/docs/components/subscribe-input' },
   { title: 'Text', href: '/docs/components/text' },
+  { title: 'Tooltip Cat', href: '/docs/components/tooltip-cat' },
+  { title: 'Voice Record', href: '/docs/components/voice-record' },
 ]
 
 export const GsapComponents = [
@@ -25,24 +26,44 @@ export const GsapComponents = [
     href: '/docs/gsap/components/color-text-reveal',
   },
   { title: 'Fancy Text Hover', href: '/docs/gsap/components/fancy-text-hover' },
+  {
+    title: 'GSAP Projects Section',
+    href: '/docs/gsap/components/gsap-projects-section',
+  },
+  { title: 'Image Trail', href: '/docs/gsap/components/image-trail' },
   { title: 'Interactive Team', href: '/docs/gsap/components/interactive-team' },
   { title: 'Link Hover', href: '/docs/gsap/components/link-hover' },
   { title: 'Magnetic Effect', href: '/docs/gsap/components/magnetic-effect' },
   {
-    title: 'Scroll Text Reveal V2',
-    href: '/docs/gsap/components/scroll-text-reveal-v2',
+    title: 'Morphing Highlighter',
+    href: '/docs/gsap/components/morphing-highlighter',
+  },
+  {
+    title: 'Perspective Story Card',
+    href: '/docs/gsap/components/perspective-story-card',
   },
   {
     title: 'Scroll Text Reveal',
     href: '/docs/gsap/components/scroll-text-reveal',
   },
+  {
+    title: 'Scroll Text Reveal V2',
+    href: '/docs/gsap/components/scroll-text-reveal-v2',
+  },
+  {
+    title: 'Sticky Cards Section',
+    href: '/docs/gsap/components/sticky-cards-section',
+  },
   { title: 'Text Reveal', href: '/docs/gsap/components/text-reveal' },
+  {
+    title: 'Title Mask Effect',
+    href: '/docs/gsap/components/title-mask-effect',
+  },
 ]
 
 export const UsefulLinks = [
-  { title: 'Home', href: '/' },
+  { title: 'Source Code', href: 'https://github.com/shatlyk1011/emerald-ui' },
   { title: 'Motion Components', href: '/docs' },
-  { title: 'GSAP Components', href: '/docs' },
+  { title: 'GSAP Components', href: '/docs/gsap' },
   { title: 'Privacy Policy', href: '/privacy-policy' },
-  { title: 'Github', href: 'https://github.com/shatlyk1011/emerald-ui' },
 ]
